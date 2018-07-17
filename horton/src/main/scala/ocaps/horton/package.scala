5a6a2908bc2f00d49fad638c047562dfac3f1538
Will Sargent <will.sargent@gmail.com>
2018-07-17 12:19:11 -0700

    Move horton code around

---
+++ horton/src/main/scala/ocaps/horton/package.scala
@@ -0,0 +1,20 @@
+package ocaps
+
+package object horton {
+  import ocaps.Brand._
+
+  trait Fill[T] extends (Stub[T] => Unit)
+
+  type FillBox[T] = Box[Fill[T]]
+
+  trait Provide[T] extends (FillBox[T] => Unit)
+
+  type Gift[T] = Box[Provide[T]]
+
+  trait Wrap[T] extends ((Stub[T], Who) => Gift[T])
+
+  trait Unwrap[T] extends ((Gift[T], Who) => Stub[T])
+
+  type Proxy[T] = T
+}
+
